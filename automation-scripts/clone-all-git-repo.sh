ab15f1d5ab037e79a439bf5c7225c275532ebcf3
root <root@localhost.localdomain>
2016-06-06 22:40:16 +0530

    Added variables and feature to have a copy of files containing the following - 1. Complete information of all each public github repositories 2. URL file containing the urls of all public github repositories to clone 3. File containing the name of repositories cloned

--- automation-scripts/clone-all-git-repo.sh
+++ automation-scripts/clone-all-git-repo.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
-#Author - Ashay Maheshwari. 
-#Purpose - This script can be used to get the list of all the private repositories of a user and clone itin your local directory 
+#Author - Ashay Maheshwari
+#Purpose - To clone all public github repositories of a github user 
 
-USER=ashay-maheshwari;
+USER=ashay-maheshwari;                   #Initialize the username or organization name 
+REPO_PATH="/root/deleteme"               #Give the base path where every repository is cloned 
+GIT_REPO_INFO="git-repo-info.txt"        #Variable contain the file name to hold all info related to git repo
+URL_TO_CLONE="url_to_clone.txt"          #Varibale contain the file name to hold urls of all repo to be cloned 
+REPOSITORY_NAMES="repository_names.txt"  #Variable contain the file name to hold the names of all repositories 
 
-#command to get info about all the repo and fetch the url to clone 
-curl "https://api.github.com/users/$USER/repos?per_page=1000" | grep "clone_url" | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > url_to_clone.txt
+curl "https://api.github.com/users/$USER/repos?per_page=1000" > $REPO_PATH/$GIT_REPO_INFO
+cat $REPO_PATH/$GIT_REPO_INFO | grep "clone_url" | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $REPO_PATH/$URL_TO_CLONE
+cat $REPO_PATH/$GIT_REPO_INFO | grep '"name"' | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $REPO_PATH/$REPOSITORY_NAMES
 
-cat url_to_clone.txt | while read url 
+cd $REPO_PATH
+cat $REPO_PATH/$URL_TO_CLONE | while read url 
 do 
    git clone $url
 done 
-	
+cat $REPO_PATH/$REPOSITORY_NAMES | while read repo_name 
+do 
+	cd $REPO_PATH/$repo_name
+	git pull
+done 
 
