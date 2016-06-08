8e4adc8cc8746fe307b5e19f315e36aaaf6373b9
root <root@localhost.localdomain>
2016-06-08 15:55:45 +0530

    Add execption checks

--- automation-scripts/clone-all-git-repo.sh
+++ automation-scripts/clone-all-git-repo.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+#Author : Ashay Maheshwari
+#Purpose : To clone all github repositories of mentioned organization 
 
+
+USAGE () {
+echo ""
+echo -e "ERROR      :  `date`  : Expects one argument. Please execute as given below \n"
+echo    "EXECUTION  :  `date`  : sh clone-repo.sh git-username"
+echo    "EXECUTION  :  `date`  : sh clone-repo.sh git-organization-name"
+echo ""
+}
+
+
+if [ "$#" -ne 1 ]; 
+then
+  USAGE 
+  exit 1
+fi
 
 USER=$1;                                 #Initialize the username or organization name 
 BKP_DIR="/root/deleteme"                 #Give the base path where every repository is cloned 
@@ -8,9 +25,17 @@ URL_TO_CLONE="url_to_clone.txt"          #Varibale contain the file name to hold
 REPOSITORY_NAMES="repository_names.txt"  #Variable contain the file name to hold the names of all repositories 
 
 
-echo "Cloning Started : `date` " > $BKP_DIR/clone_start_time.txt
+echo "INFO  :  `date`  :  Cloning Started " > $BKP_DIR/clone_start_time.txt
 
 curl "https://api.github.com/users/$USER/repos?per_page=1000" > $BKP_DIR/$GIT_REPO_INFO
+
+grep '"message": "Not Found"' $BKP_DIR/$GIT_REPO_INFO > /dev/null
+if [ $? -eq 0 ] ; 
+then
+     echo  "ERROR  :  `date`  :  Mentioned github username does not exist " 
+     exit 1 
+fi
+
 cat $BKP_DIR/$GIT_REPO_INFO | grep "clone_url" | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $BKP_DIR/$URL_TO_CLONE
 cat $BKP_DIR/$GIT_REPO_INFO | grep '"name"' | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $BKP_DIR/$REPOSITORY_NAMES
 
@@ -24,7 +49,7 @@ done
 cat $BKP_DIR/$REPOSITORY_NAMES | while read repo_name 
 do 
         cd $BKP_DIR/$repo_name
-	echo "git pull for $repo_name : `git pull`"
+	echo "INFO  :  `date`  :  git pull for $repo_name : `git pull`"
 done 
 
-echo "Cloning Done : `date` " > $BKP_DIR/clone_stop_time.txt
+echo "INFO  :  `date`  :  Cloning Done " > $BKP_DIR/clone_stop_time.txt
