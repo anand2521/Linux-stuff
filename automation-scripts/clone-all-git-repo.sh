fc02501046b4f1b7a6c925519a2df466957a8914
ashay-maheshwari <ashay@vlabs.ac.in>
2016-06-08 15:13:51 +0530

    Add commandline

--- automation-scripts/clone-all-git-repo.sh
+++ automation-scripts/clone-all-git-repo.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
-#Author - Ashay Maheshwari
-#Purpose - To clone all public github repositories of a github user. 
 
-USER=ashay-maheshwari;                   #Initialize the username or organization name 
-REPO_PATH="/root/deleteme"               #Give the base path where every repository is cloned 
+
+USER=$1;                                 #Initialize the username or organization name 
+BKP_DIR="/root/deleteme"                 #Give the base path where every repository is cloned 
 GIT_REPO_INFO="git-repo-info.txt"        #Variable contain the file name to hold all info related to git repo
 URL_TO_CLONE="url_to_clone.txt"          #Varibale contain the file name to hold urls of all repo to be cloned 
 REPOSITORY_NAMES="repository_names.txt"  #Variable contain the file name to hold the names of all repositories 
 
-curl "https://api.github.com/users/$USER/repos?per_page=1000" > $REPO_PATH/$GIT_REPO_INFO
-cat $REPO_PATH/$GIT_REPO_INFO | grep "clone_url" | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $REPO_PATH/$URL_TO_CLONE
-cat $REPO_PATH/$GIT_REPO_INFO | grep '"name"' | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $REPO_PATH/$REPOSITORY_NAMES
 
-cd $REPO_PATH
-cat $REPO_PATH/$URL_TO_CLONE | while read url 
+echo "Cloning Started : `date` " > $BKP_DIR/clone_start_time.txt
+
+curl "https://api.github.com/users/$USER/repos?per_page=1000" > $BKP_DIR/$GIT_REPO_INFO
+cat $BKP_DIR/$GIT_REPO_INFO | grep "clone_url" | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $BKP_DIR/$URL_TO_CLONE
+cat $BKP_DIR/$GIT_REPO_INFO | grep '"name"' | awk '{print $2}' | sed 's/\"//g' | sed 's/,//g' > $BKP_DIR/$REPOSITORY_NAMES
+
+cd $BKP_DIR
+cat $BKP_DIR/$URL_TO_CLONE | while read url 
 do 
    git clone $url
-done 
-cat $REPO_PATH/$REPOSITORY_NAMES | while read repo_name 
-do 
-	cd $REPO_PATH/$repo_name
-	git pull
+   echo ""                                #Just for a blank line             
 done 
 
+cat $BKP_DIR/$REPOSITORY_NAMES | while read repo_name 
+do 
+        cd $BKP_DIR/$repo_name
+	echo "git pull for $repo_name : `git pull`"
+done 
+
+echo "Cloning Done : `date` " > $BKP_DIR/clone_stop_time.txt
